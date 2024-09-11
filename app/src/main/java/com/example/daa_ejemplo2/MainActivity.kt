4d5f8f76c18376fc870256284c03d3c9c3b8274d
nefi2307 <alu.21130937@correo.itlalaguna.edu.mx>
2024-09-11 10:59:01 -0600

    21130937 Isaac Neftali Burciaga Chacon

--- app/src/main/java/com/example/daa_ejemplo2/MainActivity.kt
+++ app/src/main/java/com/example/daa_ejemplo2/MainActivity.kt
@@ -9,7 +9,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -34,77 +37,98 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.padding(),
                     color = MaterialTheme.colorScheme.background
-                ){
+                ) {
+                    imageAndText(string = "")
 
-                }
                 }
             }
         }
-    }
-
-
-@Composable
-fun image(name: String, modifier: Modifier = Modifier) {
-    Column (modifier = Modifier, verticalArrangement = Arrangement.Center){
-        val image = painterResource(id = R.drawable.image)
-        Image(
-            painter = image,
-            contentDescription = null,
-            contentScale = ContentScale.Crop
-        )
     }
 }
 
 
 @Composable
-fun imageAndText(modifier: Modifier = Modifier, string: String )
-{
-    Column (
-    verticalArrangement =Arrangement.Center,
-    modifier = modifier.padding(
-        start = 16.dp,
-        top = 16.dp,
-        end = 2.dp,
-        bottom = 16.dp)
-    ){
-        image(name = "hi")
-            Text(
-                text = "Instituto Tecnológico de la Laguna",
-                lineHeight = 30.sp,
-                textAlign = TextAlign.Center,
-                fontSize = 6.sp,
-                modifier = Modifier
-                    .padding(start = 16.dp,
-                        top = 16.dp,
-                        end = 16.dp)
+fun image(name: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 
-            )
-        Text(
-            text = "El Instituto Tecnológico de la Laguna es una universidad pública ubicada en la ciudad de Torreón. El Instituto Tecnológico de la Laguna forma parte del Tecnológico Nacional de México",
-            lineHeight = 10.sp,
-            textAlign = TextAlign.Center,
-            fontSize = 6.sp,
+    }
+}
+
+
+@Composable
+fun imageAndText(modifier: Modifier = Modifier, string: String) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .padding(
+                //start = 16.dp,
+                top = 16.dp,
+                end = 2.dp,
+                bottom = 16.dp
+            ).fillMaxSize()
+    ) {
+        val image = painterResource(id = R.drawable.image)
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
             modifier = Modifier
-                .padding(start = 16.dp,
-                    top = 16.dp,
-                    end = 16.dp,
-                    bottom = 16.dp)
         )
         Text(
-            text = "El Instituto Tecnológico Regional De La Laguna, iniciando sus actividades el 26 de Septiembre de\n" +
-                    "1965. A nivel medio superior se comenzó en 1965 con el Bachillerato de Ciencias y Tecnologia en tres opciones: Electrica, Mecanica automotriz (combustión interna) y Mecánica (máquinas y herramientas). La población escolar ascendió a 352 estudiantes inscritos en los siguientes niveles\n" +
-                    "152 en medio superior, 148 en tecnico industral y\n" +
-                    "52 técnicos agropecuarios, atendidos por 2 directores, 21 docentes y 8 personal\n" +
-                    "aoministrativo",
+            text = "Instituto Tecnológico de la Laguna",
+            //lineHeight = 30.sp,
+            textAlign = TextAlign.Center,
+            fontSize = 16.sp,
+            modifier = Modifier
+                .padding(
+                    start = 16.dp,
+                    top = 16.dp,
+                    end = 16.dp
+                )
+
+        )
+        Text(
+            text = "El Instituto Tecnológico de la Laguna es una \n " +
+                    "universidad pública ubicada en la ciudad de Torreón. \n" +
+                    "El Instituto Tecnológico de la Laguna \n" +
+                    "forma parte del Tecnológico Nacional de México \n",
+            //lineHeight = 10.sp,
+            textAlign = TextAlign.Center,
+            fontSize = 10.sp,
+            modifier = Modifier
+                .padding(
+                    //start = 16.dp,
+                    top = 16.dp
+                )
+        )
+        Text(
+            text = "El Instituto Tecnológico Regional De La Laguna \n" +
+                    ", iniciando sus actividades el 26 de Septiembre de \n" +
+                    " 1965. A nivel medio superior se comenzó en \n" +
+                    " 1965 con el Bachillerato de Ciencias y Tecnologia en \n" +
+                    " tres opciones: Electrica, Mecanica automotriz \n" +
+                    " (combustión interna) y Mecánica (máquinas y \n" +
+                    "herramientas). La población escolar ascendió a \n" +
+                    "352 estudiantes inscritos en los siguientes niveles \n" +
+                    " 152 en medio superior, 148 en tecnico industral y \n" +
+                    "52 técnicos agropecuarios, atendidos por 2 \n" +
+                    "directores, 21 docentes y 8 personal aoministrativo\n",
 
             textAlign = TextAlign.Left,
-            lineHeight = 10.sp,
-            fontSize = 6.sp,
+            //lineHeight = 10.sp,
+            fontSize = 10.sp,
             modifier = Modifier
-                .padding(start = 16.dp,
+                .padding(
+                    start = 16.dp,
                     top = 16.dp,
                     end = 16.dp,
-                    bottom = 16.dp)
+                    bottom = 16.dp
+                )
         )
     }
 }
