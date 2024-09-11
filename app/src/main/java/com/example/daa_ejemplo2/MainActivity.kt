85999bfa340640dbc332c554896db320859bc369
sebdev16 <sebapplelife@gmail.com>
2024-09-11 08:21:22 -0600

    practica 2, 21130572 Sebastián Aguirre Jasso

--- app/src/main/java/com/example/daa_ejemplo2/MainActivity.kt
+++ app/src/main/java/com/example/daa_ejemplo2/MainActivity.kt
@@ -26,7 +26,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.daa_ejemplo2.ui.theme.DAAEjemplo2Theme
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -132,6 +131,7 @@ fun GreetingShow(
 
     }
 }
+//Sebastian
 
 @Preview(showBackground = true)
 @Composable
