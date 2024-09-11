6750558baf35287eee12572bb7babeb093b048bb
nefi2307 <alu.21130937@correo.itlalaguna.edu.mx>
2024-09-10 19:51:19 -0600

    Imagen añadida con cambios en el código de la funcion imageAndText.

--- app/src/main/java/com/example/daa_ejemplo2/MainActivity.kt
+++ app/src/main/java/com/example/daa_ejemplo2/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.daa_ejemplo2.ui.theme.DAAEjemplo2Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,16 +30,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DAAEjemplo2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ){
+
+                }
                 }
             }
         }
     }
-}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -38,10 +50,45 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+
+@Composable
+fun imageAndText(modifier: Modifier = Modifier, message:String, string: String )
+{
+
+    val imagen = painterResource(id = R.drawable.image)
+    Box{
+        Image(
+            painter = imagen,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+
+            )
+    }
+
+        Column(verticalArrangement = Arrangement.Center, modifier = modifier.padding(8.dp)){
+            
+            Text(
+                text = message,
+                fontSize = 5.sp,
+                lineHeight = 116.sp,
+                textAlign = TextAlign.Center
+            )
+            Text(
+                text = string,
+                fontSize = 5.sp,
+                lineHeight = 116.sp,
+                textAlign = TextAlign.Center
+            )
+
+        }
+
+
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     DAAEjemplo2Theme {
-        Greeting("Android")
+            imageAndText(message = "Instituto Tecnológico de la Laguna", string = "El instituto tecnologico de la laguna es una universidad publica ubicada en la ciudad de Torreón. El instituto Tecnológico de la Laguna forma parte del Tecnológico Nacional de México.")
     }
 }
